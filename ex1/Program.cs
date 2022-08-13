81f9fa7cb6731561bfd34db9cb964244aaa58933
escogera <maostyle@mail.ru>
2022-08-13 08:48:36 +0300

    ex1 complete

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, -567, 89, 223-> 3
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+Console.Write("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[size];
+int count = 0;
+     for(int i = 0; i < size; i++)
+        {
+            Console.Write($"A[{i}] = ");
+            massive[i] = Convert.ToInt32(Console.ReadLine());
+            if (massive[i]==0)count=count+1;    
+        }                  
+Console.WriteLine();
+PrintArray(massive);
+Console.WriteLine();
+if (count==0) Console.WriteLine ("Пользователь не вводил цифру 0");               
+ else Console.WriteLine ($"Количество введенного числа 0 полльзователем равно - {count}"); 
+ 
+
+
